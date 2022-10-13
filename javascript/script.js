12ad4ad8a985aa9888fc295fcc76a590f09cfe00
sergiorodriguesh <sergiorodriguesh@gmail.com>
2022-10-13 17:24:07 -0300

    bugs e estilo alterados

--- javascript/script.js
+++ javascript/script.js
@@ -1,33 +1,61 @@
 function inserir(){
-    var inputElement =  document.querySelector("#valor")
+  var vagas = document.querySelector("#valor").value;
+  var res = document.querySelector("#resultado")
 
-    console.log(inputElement.value);
-
-    document.querySelector("#resultado").textContent = inputElement.value + '\n' + 'Vagas disponiveis';
+  if(vagas == ""){
+    alert("[ERRO] Por favor digite um número!")
+  } else if(vagas == 0){
+    alert('Por favor digite um numero diferente de 0')
+    document.querySelector("#valor").value = "";
+  } else {
+    res.textContent = `${vagas}`
+    res.innerHTML += "<br>Vagas disponiveis"
+  }
 }
 
-function entrou (){
-    const resultadoElement = document.querySelector("#resultado");
+function entrou(){
+  var vagas = document.querySelector("#valor").value;
+      if(vagas.length == 0 || vagas.length == ""){
+        alert('Por favor insira um numero antes de usar esse botão')
+      } else {
+        var elementResultado = document.querySelector("#resultado");
+        var valorNovo = parseInt(elementResultado.textContent) - 1;
+        if(valorNovo == vagas){
+            alert('ERRO vagas alcançadas')
+            elementResultado.textContent = Number(valorNovo) - 1
+            elementResultado.innerHTML += "<br> Vagas disponiveis"
+        }else if (valorNovo <= -1){
+          alert('Numero de vagas zerado')
+        } else {
+          elementResultado.textContent = valorNovo;
+          elementResultado.innerHTML += "<br> Vagas disponiveis"
+        }
+      }
+    }
 
-    const novoValor = parseInt(resultadoElement.textContent)-1 + '\n' + 'Vagas disponiveis';
-    
-    resultadoElement.textContent = novoValor;
-
-    console.log(novoValor)
-}
-
-function saiu (){
-    const resultadoElement = document.querySelector("#resultado");
-
-    const novoValor = parseInt(resultadoElement.textContent)+1 + '\n' + 'Vagas disponiveis';
-
-    resultadoElement.textContent = novoValor;
-
-    console.log(novoValor)
+function saiu(){
+  var vagas = document.querySelector("#valor").value;
+    if(vagas.length == 0 || vagas.length == ""){
+      alert('Por favor insira um numero antes de usar esse botão')
+    } else {
+      var elementResultado = document.querySelector("#resultado");
+      var valorNovo = parseInt(elementResultado.textContent) + 1;
+      if(valorNovo > vagas){
+        alert('Limite de vagas alcançado')
+      } else {
+        elementResultado.textContent = valorNovo;
+        elementResultado.innerHTML += "<br> Vagas disponiveis"
+      }
+      
+    }
 }
 
 function limpar(){
-    document.querySelector("#valor").value = '';
-    document.querySelector("#valor").textContent = '';
-    document.querySelector("#resultado").textContent = '';
+  var vagas = document.querySelector("#valor").value;
+  if(vagas.length == ""){
+    alert('Nao tem oq limpar')
+  } else {
+    document.querySelector("#valor").value = "";
+    document.querySelector("#resultado").textContent = "";
+  }
 }
