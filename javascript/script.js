a3249c205f74bd6da1d518e2968e28e76816b825
Sergio <sergiohrodriguess@gmail.com>
2023-01-09 18:57:08 -0300

    erro ao passar das vagas

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,11 @@
+/*------------BOTAO INSERIR----------*/
 const btnInserir = document.querySelector("#btnInserir");
 const valorInpt = document.querySelector("#valorInpt");
 const res = document.querySelector("#res");
 const principal = document.querySelector("#principal");
 const secundaria = document.querySelector(".secundaria");
-const error = document.querySelector("#principal__erro")
+const error = document.querySelector("#principal__erro");
+
 
 function verificarInput(){
   if(valorInpt.value === ""){
@@ -22,27 +24,43 @@ btnInserir.addEventListener("click", verificarInput)
 
 
 
+/*--------------BOTAO ENTROU--------------*/
 const btnEntrou = document.querySelector("#btnEntrou")
 const arrowDown = document.querySelector("#arrow-down");
+const mensagemDeErroVagas = document.querySelector(".mensagem-erro-vagas")
+
 
 function entrou(){
   let resultado = document.querySelector("#res");
   let valorNovo = parseInt(resultado.textContent) - 1;
   resultado.textContent = valorNovo
+
   if(valorNovo < 0){
-    alert("Numero de vagas zerado, espere sair alguém")
+    mensagemDeErroVagas.style.visibility = "visible"
+    mensagemDeErroVagas.innerHTML = "Sem vagas, espere alguém sair."
     resultado.textContent = 0;
-  } else {
+  }else {
     arrowDown.style.visibility = "visible";
     arrowUp.style.visibility = "collapse";
   }
+  
+  const valorInputTransformadoEmNumber = parseInt(valorInpt.value);
+  
+  if(valorNovo < valorInputTransformadoEmNumber){
+    // mensagemDeErroVagas.style.visibility = "collapse"
+    mensagemDeErroLimite.style.visibility = "hidden"
+  }
+  
 }
 
 btnEntrou.addEventListener('click', entrou)
 
 
+
+/*----------BOTAO SAIU-----------------*/
 const btnSaiu = document.querySelector("#btnSaiu")
 const arrowUp = document.querySelector("#arrow-up")
+const mensagemDeErroLimite = document.querySelector(".mensagem-erro-limite")
 
 function saiu(){
   let resultado = document.querySelector("#res");
@@ -50,11 +68,24 @@ function saiu(){
   resultado.textContent = valorNovo
   arrowUp.style.visibility = "visible"
   arrowDown.style.visibility = "collapse"
+  
+  if(valorNovo !== 0){
+    mensagemDeErroVagas.style.visibility = "collapse"
+  }
+
+  const valorInputTransformadoEmNumber = parseInt(valorInpt.value);
+  
+  if(valorNovo > valorInputTransformadoEmNumber){
+    resultado.textContent = valorInputTransformadoEmNumber
+    mensagemDeErroLimite.style.visibility = "visible"
+    mensagemDeErroLimite.innerHTML = "Limite de vagas do estabelecimento"
+  }
 }
 
 btnSaiu.addEventListener('click', saiu)
 
 
+/*----------BOTAO VOLTAR-----------------*/
 const btnVoltar = document.querySelector("#btnVoltar");
 
 function voltar(){
@@ -62,86 +93,3 @@ function voltar(){
 }
 
 btnVoltar.addEventListener("click", voltar)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function inserir(){
-//   const principal = document.querySelector("#principal");
-//   const secundaria = document.querySelector("#secundaria")
-//   const vagas = document.querySelector("#valor").value;
-//   const res = document.querySelector("#resultado")
-
-
-//   if(vagas == ""){
-//     alert("[ERRO] Por favor digite o numero de vagas!")
-//   } else if(vagas == 0){
-//     alert('Por favor digite um numero diferente de 0')
-//     document.querySelector("#valor").value = "";
-//   } else {
-//     principal.style.display = "none";
-//     secundaria.style.display = "block";
-//     res.textContent = `${vagas} <br>Vagas disponiveis`;
-//   }
-// }
-
-// function entrou(){
-//   var vagas = document.querySelector("#valor").value;
-//       if(vagas.length == 0 || vagas.length == ""){
-//         alert('Por favor insira um numero antes de usar esse botão')
-//       } else {
-//         var elementResultado = document.querySelector("#resultado");
-//         var valorNovo = parseInt(elementResultado.textContent) - 1;
-//         if(valorNovo == vagas){
-//             alert('ERRO vagas alcançadas')
-//             elementResultado.textContent = Number(valorNovo) - 1
-//             elementResultado.innerHTML += "<br> Vagas disponiveis"
-//         }else if (valorNovo <= -1){
-//           alert('Numero de vagas zerado')
-//         } else {
-//           elementResultado.textContent = valorNovo;
-//           elementResultado.innerHTML += "<br> Vagas disponiveis"
-//         }
-//       }
-//     }
-
-// function saiu(){
-//   var vagas = document.querySelector("#valor").value;
-//     if(vagas.length == 0 || vagas.length == ""){
-//       alert('Por favor insira um numero antes de usar esse botão')
-//     } else {
-//       var elementResultado = document.querySelector("#resultado");
-//       var valorNovo = parseInt(elementResultado.textContent) + 1;
-//       if(valorNovo > vagas){
-//         alert('Limite de vagas alcançado')
-//       } else {
-//         elementResultado.textContent = valorNovo;
-//         elementResultado.innerHTML += "<br> Vagas disponiveis"
-//       }
-      
-//     }
-// }
-
-// function limpar(){
-//   var vagas = document.querySelector("#valor").value;
-//   if(vagas.length == ""){
-//     alert('Não tem dados para limpar')
-//   } else {
-//     document.querySelector("#valor").value = "";
-//     document.querySelector("#resultado").textContent = "";
-//   }
-// }
