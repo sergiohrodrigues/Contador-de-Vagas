e5c6271a9f52814aec16f54b67eb4566b01de926
Sergio <sergiohrodriguess@gmail.com>
2023-06-08 13:51:49 -0300

    settimeout em erros

--- javascript/script.js
+++ javascript/script.js
@@ -6,21 +6,26 @@ const principal = document.querySelector("#principal");
 const secundaria = document.querySelector(".secundaria");
 const error = document.querySelector("#principal__erro");
 
+btnInserir.addEventListener("click", verificarInput)
 
 function verificarInput(){
-  if(valorInpt.value === ""){
-    error.style.display = "block";
-  } else if(valorInpt.value == 0){
-    error.innerHTML = "Digite um numero diferente de 0"
-    error.style.display = "block";
+  if(valorInpt.value < 0){
+    alert('Por favor digite valores somente positivos')
+    valorInpt.value = ''
+    valorInpt.focus()
   } else {
-    res.innerHTML = `${valorInpt.value}`;
-    principal.style.display = "none";
-    secundaria.style.display = "block";
+    if(valorInpt.value === ""){
+      error.style.display = "block";
+    } else if(valorInpt.value == 0){
+      error.innerHTML = "Digite um numero diferente de 0"
+      error.style.display = "block";
+    } else {
+      res.innerHTML = `${valorInpt.value}`;
+      principal.style.display = "none";
+      secundaria.style.display = "block";
+    }
   }
 }
-
-btnInserir.addEventListener("click", verificarInput)
 
 
 
@@ -39,9 +44,11 @@ function entrou(){
     mensagemDeErroVagas.style.visibility = "visible"
     mensagemDeErroVagas.innerHTML = "Sem vagas, espere alguém sair."
     resultado.textContent = 0;
+    setTimeout(tirarErroSemVagas, 3000);
   }else {
     arrowDown.style.visibility = "visible";
     arrowUp.style.visibility = "collapse";
+    setTimeout(tirarFlechaVermelha, 3000)
   }
   
   const valorInputTransformadoEmNumber = parseInt(valorInpt.value);
@@ -51,6 +58,14 @@ function entrou(){
     mensagemDeErroLimite.style.visibility = "hidden"
   }
   
+}
+
+function tirarErroSemVagas(){
+  mensagemDeErroVagas.style.visibility = "hidden"
+}
+
+function tirarFlechaVermelha(){
+  arrowDown.style.visibility = "hidden";
 }
 
 btnEntrou.addEventListener('click', entrou)
@@ -68,18 +83,28 @@ function saiu(){
   resultado.textContent = valorNovo
   arrowUp.style.visibility = "visible"
   arrowDown.style.visibility = "collapse"
-  
+  setTimeout(tirarFlechaVerde, 3000)
   if(valorNovo !== 0){
     mensagemDeErroVagas.style.visibility = "collapse"
   }
-
+  
   const valorInputTransformadoEmNumber = parseInt(valorInpt.value);
   
   if(valorNovo > valorInputTransformadoEmNumber){
     resultado.textContent = valorInputTransformadoEmNumber
+    arrowUp.style.visibility = "hidden"
     mensagemDeErroLimite.style.visibility = "visible"
     mensagemDeErroLimite.innerHTML = "Limite de vagas atingido"
+    setTimeout(tirarErroLimiteDeVagas, 3000)
   }
+}
+
+function tirarErroLimiteDeVagas(){
+  mensagemDeErroLimite.style.visibility = "hidden"
+}
+
+function tirarFlechaVerde(){
+  arrowUp.style.visibility = "hidden"
 }
 
 btnSaiu.addEventListener('click', saiu)
